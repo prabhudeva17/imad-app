56b151160d0ca02328f42e4a06a9b082b81bbb08
prabhudeva17 <30696072+prabhudeva17@users.noreply.github.com>
2017-08-14 11:03:13 +0000

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,13 +10,13 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function (req, res){
-    req.send('Article-one is requested and will be served here');
+    res.send('Article-one is requested and will be served here');
 });
 app.get('/article-two',function (req, res){
-    req.send('Article-two is requested and will be served here');
+    res.send('Article-two is requested and will be served here');
 });
 app.get('/article-three',function (req, res){
-    req.send('Article-three is requested and will be served here');
+    res.send('Article-three is requested and will be served here');
 });
 
 app.get('/ui/style.css', function (req, res) {
