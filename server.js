b49dcf321d79828a3c277f4eafd82aea46cdbfb1
prabhudeva17 <30696072+prabhudeva17@users.noreply.github.com>
2017-08-20 09:30:46 +0000

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/ui/profile', function (req, res) {
+app.get('/profile', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'profile.html'));
 });
 
