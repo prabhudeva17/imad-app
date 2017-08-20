cc0479133cd2c4a202c8623223788616688c2b51
prabhudeva17 <30696072+prabhudeva17@users.noreply.github.com>
2017-08-20 09:21:31 +0000

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get ('/a1',function (req,res){
+    res.send('a1 is the powerful article');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
